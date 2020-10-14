4bf21ce5d269050406aea087684741b99c2131be
Erika Nakagawa <enakagawa@middlebury.edu>
2020-10-14 15:47:25 -0400

    started

--- client/src/image/imageencoder.py
+++ client/src/image/imageencoder.py
@@ -2,4 +2,12 @@
 
 import cv2
 
-img = cv2.imread()
+img = cv2.imread('nakagawa.jpg')
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+
+def lsb(image, message):
+    for row in image:
+        for pixel in row:
+            
