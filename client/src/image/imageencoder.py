6feebdf9c8c1819ab6a27a315c544ce7b2cd348e
Lizeth Lucero <lizethlucero@yahoo.com>
2020-10-21 16:22:35 -0400

    Almost done with lsb

--- client/src/image/imageencoder.py
+++ client/src/image/imageencoder.py
@@ -4,8 +4,8 @@ import cv2
 
 img = cv2.imread('nakagawa.jpg')
 
-cv2.imshow('image', img)
-cv2.waitKey(0)
+# cv2.imshow('image', img)
+# cv2.waitKey(0)
 
 
 def textToBinary(x):
@@ -15,19 +15,36 @@ def textToBinary(x):
         result = result + (format(ascii_num, 'b'))
     return result
 
-# print(str(textToBinary("hH")))
-
 def lsb(image, message):
     msg = textToBinary(message)
+    print("msg is : "+ msg)
+
     msg_len = len(msg)
     pixels_len = image.size // 3
+    img_copy = image
+    i = 0
+    for mat in img_copy:
+        for pixel in mat:
+            print("First pixel is : "+str(pixel))
+            for rgb_val in range(0,3):
+                if i < msg_len:
+                    print("i is " , msg[i])
+                    binNum = bin(pixel[rgb_val])
+                    print("Previous RGB : " , pixel[rgb_val] , ", Pre RGB Bin: " , binNum)
+                    newBin = binNum[2:-1] + msg[i]
+                    pixel[rgb_val] = int(newBin, 2)
+                    print("New RGB : " , pixel[rgb_val] , ", New RGB Bin: " , newBin)
+                    i+=1
+                else:
+                    break
+            print("New pixel is : " +str(pixel))
 
-    if pixels_len >= msg_len:
-        for row in image:
-            for col in range(0,3):
-                print(image[row])
+    
+    if img_copy.all != img.all:
+        print("Hooray!")
     else:
-        print("Message exceeds size limit")
+        print("cry")
 
+    # print(image)
     
 print(lsb(img, "Hi"))
