a6a76f66a550db13b8f5319af74a56b841beb01c
Lizeth Lucero <lizethlucero@yahoo.com>
2020-10-21 13:39:47 -0400

    starting lsb

--- client/src/image/imageencoder.py
+++ client/src/image/imageencoder.py
@@ -18,6 +18,16 @@ def textToBinary(x):
 # print(str(textToBinary("hH")))
 
 def lsb(image, message):
-    for row in image:
-        for pixel in row:
-            
+    msg = textToBinary(message)
+    msg_len = len(msg)
+    pixels_len = image.size // 3
+
+    if pixels_len >= msg_len:
+        for row in image:
+            for col in range(0,3):
+                print(image[row])
+    else:
+        print("Message exceeds size limit")
+
+    
+print(lsb(img, "Hi"))
