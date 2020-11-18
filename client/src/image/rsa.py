05d01c2c23df151f6296047264c8b39533b2d532
Erika Nakagawa <enakagawa@middlebury.edu>
2020-11-18 10:47:18 -0500

    forgot to add rsa

---
+++ client/src/image/rsa.py
@@ -0,0 +1,48 @@
+# need to install pycrypto module
+# using pip install pycrypto
+# https://pypi.org/project/pycrypto/
+# https://www.dlitz.net/software/pycrypto/api/current/
+
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto import Random
+import base64
+from pprint import pprint
+
+def makeRSAkeys():
+    length = 1024 #bits
+    privatekey = RSA.generate(length, Random.new().read)
+    publickey = privatekey.publickey()
+    return privatekey, publickey
+
+def encrypt(publickey, message):
+    cipher_text = publickey.encrypt(message, 32)[0]
+    b64cipher = base64.b64encode(cipher_text)
+    return b64cipher
+
+def decrypt(privatekey, b64cipher):
+    decoded_ciphertext = base64.b64decode(b64cipher)
+    message = privatekey.decrypt(decoded_ciphertext)
+    return message
+
+def main():
+    privatekey, publickey = makeRSAkeys()
+    print("Public key pair:  ")
+    print(f"(n = {bin(publickey.n)}, e = {bin(publickey.e)})")
+
+    message = b'Yay'
+
+    print("Message to encrypt: ")
+    print("Yay")
+    encrypted = encrypt(publickey, message)
+    #print(encrypted)
+
+    print("Encrypted: ")
+    print(str(encrypted).strip('b'))
+    decrypted = decrypt(privatekey, encrypted)
+    #print(decrypted)
+
+    msg = str(decrypted)
+    #print(msg.strip('b'))
+
+main()
