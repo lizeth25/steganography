44013ebab866afc0a222c6cebec66cd80696be8a
Lizeth Lucero <lizethlucero@yahoo.com>
2020-12-03 13:26:25 -0500

    server started

---
+++ server/hello.py
@@ -0,0 +1,14 @@
+
+import json
+
+d = { "text": "hello", "author" : "jonas"}
+y = json.dumps(d)
+
+print(y)
+
+# import sys
+
+# if __name__ == "__main__":
+#     st = sys.argv[1]
+
+#     print(st + "from py")
