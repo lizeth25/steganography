125db75510430b1afedd6b823a7ffec0e1a1542d
Lizeth Lucero <lizethlucero@yahoo.com>
2020-12-06 16:07:29 -0500

    server updates

--- server/routes.test.js
+++ server/routes.test.js
@@ -1,9 +1,9 @@
-const { app } = require('./routes');
+const { app } = require("./routes");
 
 test('Server "smoke" test', () => {
   expect(app).toBeDefined();
 });
 
-test('dotenv configured', () => {
-  expect(process.env.MY_SECRET).toBeDefined();
-});
+// test('dotenv configured', () => {
+//   expect(process.env.MY_SECRET).toBeDefined();
+// });
